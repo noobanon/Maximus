7970092568c2ab3b9a9158b66a82bc90f8caf2b2
MR ANON <51384033+noobanon@users.noreply.github.com>
2020-10-13 18:01:28 +0530

    Update checkers.py

--- userbot/modules/checkers.py
+++ userbot/modules/checkers.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot import bot, CMD_HELP
+from userbot import bot
 from userbot.events import register
 
 #Module By @noobanon thnxkk bye :)
