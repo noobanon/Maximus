6599d261d9bcabbb8dd93aa2d5b8bead030b99be
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-27 13:07:17 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -4,15 +4,18 @@
 # you may not use this file except in compliance with the License.
 #
 
-from telethon import events
+from telethon import events 
 from userbot import bot
 
 
 def register(**args):
     pattern = args.get('pattern', None)
+    groups_only = args.get('groups_only', False)
 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
+    if "groups_only" in args:
+        del args['groups_only']
 
     def decorator(func):
         bot.add_event_handler(func, events.NewMessage(**args))
