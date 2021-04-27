8e337b3c028338545f56d478ef15356d63845f8b
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-27 14:25:57 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -11,124 +11,14 @@ from userbot import bot, LOGGER_GROUP, LOGGER
 
 
 def register(**args):
-    """ Register a new event. """
-    pattern = args.get("pattern", None)
-    disable_edited = args.get("disable_edited", False)
-    ignore_unsafe = args.get("ignore_unsafe", False)
-    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
-    group_only = args.get("group_only", False)
-    disable_errors = args.get("disable_errors", False)
-    insecure = args.get("insecure", False)
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
+    pattern = args.get('pattern', None)
 
-    if "disable_edited" in args:
-        del args["disable_edited"]
-
-    if "ignore_unsafe" in args:
-        del args["ignore_unsafe"]
-
-    if "group_only" in args:
-        del args["group_only"]
-
-    if "disable_errors" in args:
-        del args["disable_errors"]
-
-    if "insecure" in args:
-        del args["insecure"]
-
-    if pattern:
-        if not ignore_unsafe:
-            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
 
     def decorator(func):
-        async def wrapper(check):
-            if check.edit_date and check.is_channel and not check.is_group:
-                # Messages sent in channels can be edited by other users.
-                # Ignore edits that take place in channels.
-                return
-            if group_only and not check.is_group:
-                await check.respond("`Are you sure this is a group?`")
-                return
-            if check.via_bot_id and not insecure and check.out:
-                # Ignore outgoing messages via inline bots for security reasons
-                return
-
-            try:
-                await func(check)
-            #
-            # HACK HACK HACK
-            # Raise StopPropagation to Raise StopPropagation
-            # This needed for AFK to working properly
-            # TODO
-            # Rewrite events to not passing all exceptions
-            #
-            except events.StopPropagation:
-                raise events.StopPropagation
-            # This is a gay exception and must be passed out. So that it doesnt spam chats
-            except KeyboardInterrupt:
-                pass
-            except BaseException as e:
-                LOGGER.exception(e)  # Log the error in console
-                # Check if we have to disable error logging message.
-                if not disable_errors:
-                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-                    text = "**Sorry, I encountered an error!**\n"
-                    link = "[https://t.me/ThebotSupport](Userbot Support Chat)"
-                    text += "If you want to report it, "
-                    text += f"just forward this message to {link}.\n"
-                    text += "I won't log anything except the fact of error and date.\n"
-
-                    ftext = "\nDisclaimer:\nThis file is uploaded ONLY here, "
-                    ftext += "we logged only fact of error and date, "
-                    ftext += "we respect your privacy. "
-                    ftext += "You may not report this error if you have "
-                    ftext += "any confidential data here. No one will see your data "
-                    ftext += "if you choose not to do so.\n\n"
-                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
-                    ftext += "\nDate: " + date
-                    ftext += "\nGroup ID: " + str(check.chat_id)
-                    ftext += "\nSender ID: " + str(check.sender_id)
-                    ftext += "\n\nEvent Trigger:\n"
-                    ftext += str(check.text)
-                    ftext += "\n\nTraceback info:\n"
-                    ftext += str(format_exc())
-                    ftext += "\n\nError text:\n"
-                    ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
-
-                    command = 'git log --pretty=format:"%an: %s" -5'
-
-                    ftext += "\n\n\nLast 5 commits:\n"
-
-                    process = await asyncsubshell(
-                        command, stdout=asyncsub.PIPE, stderr=asyncsub.PIPE
-                    )
-                    stdout, stderr = await process.communicate()
-                    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-                    ftext += result
-
-                    with open("error.log", "w+") as output_file:
-                        output_file.write(ftext)
-
-                    if LOGGER:
-                        await check.client.send_file(
-                            LOGGER_GROUP, "error.log", caption=text
-                        )
-                    else:
-                        await check.client.send_file(
-                            check.chat_id, "error.log", caption=text
-                        )
-
-                    remove("error.log")
-            else:
-                pass
-
-        if not disable_edited:
-            bot.add_event_handler(wrapper, events.MessageEdited(**args))
-        bot.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
+        bot.add_event_handler(func, events.NewMessage(**args))
+        bot.add_event_handler(func, events.MessageEdited(**args))
+        return func
 
     return decorator
