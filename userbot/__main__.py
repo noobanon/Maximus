57c39001a32dbbef8d65e4f1b79b69f41e009e33
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-27 15:53:56 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -18,7 +18,7 @@ for module_name in ALL_MODULES:
     imported_module = importlib.import_module("userbot.modules." + module_name)
 
 LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          "Should you need assistance, head to https://t.me/userbot_support")
+          "Should you need assistance, head to https://t.me/TheBotSupport")
 LOGS.info("Your Bot Version is 2.2-a")
 
 if len(sys.argv) not in (1, 3, 4):
