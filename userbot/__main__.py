d26da08b9e2bb24f2f61562b51cb9c356ba4025a
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-29 01:20:10 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,7 +11,7 @@ bot.start()
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("userbot.modules." + module_name)
 
-LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
+LOGS.info("Your Bot is alive! Test it by typing .alive or .ping on any chat."
           "Should you need assistance, head to https://t.me/TheBotSupport")
 LOGS.info("Your Bot Version is 2.2 By @noobanon")
 
