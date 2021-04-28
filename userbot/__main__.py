742339848575bc7759210fb45777722b4146b6d8
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-29 00:25:54 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,17 +12,14 @@ from userbot import BRAIN_CHECKER, LOGS, bot, LOGGER_GROUP
 from userbot.modules import ALL_MODULES
 
 
-bot.start()
-
-for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("userbot.modules." + module_name)
-
-LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          "Should you need assistance, head to https://t.me/TheBotSupport")
-LOGS.info("Your Bot Version is 2.2-a")
-
-if len(sys.argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.send_message(LOGGER_GROUP, "Userbot is alive")
-    bot.run_until_disconnected()
+async def main():
+    await bot.start()
+    for module_name in ALL_MODULES:
+        imported_module = importlib.import_module("userbot.modules." + module_name)
+        LOGS.info("Your Bot is alive! Test it by typing .alive on any chat." "Should you need assistance, head to https://t.me/TheBotSupport")
+        LOGS.info("Your Bot Version is 2.2-a")
+        if len(sys.argv) not in (1, 3, 4):
+            bot.disconnect()
+    else:
+        await bot.send_message(LOGGER_GROUP, "Connection is Alive!")
+    await bot.run_until_disconnected()
