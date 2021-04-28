8279274211fcfe1b0f657b18d5a5f2c1e658fcc8
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-29 00:37:58 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,9 +11,8 @@ import sys
 from userbot import BRAIN_CHECKER, LOGS, bot, LOGGER_GROUP
 from userbot.modules import ALL_MODULES
 
-
+bot.start()
 async def main():
-    bot.start()
     for module_name in ALL_MODULES:
         imported_module = importlib.import_module("userbot.modules." + module_name)
         LOGS.info("Your Bot is alive! Test it by typing .alive on any chat." "Should you need assistance, head to https://t.me/TheBotSupport")
