d2348361f56056eba48c451ce8da47996acf6974
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-04-29 00:48:18 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,24 +1,21 @@
-# Copyright (C) 2018 Raphielscape LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.0 (the "License");
-# you may not use this file except in compliance with the License.
-#
-
 import importlib
 import sqlite3
 import sys
 
-from userbot import BRAIN_CHECKER, LOGS, bot, LOGGER_GROUP
+from userbot import BRAIN_CHECKER, LOGS, bot
 from userbot.modules import ALL_MODULES
 
+
 bot.start()
-async def main():
-    for module_name in ALL_MODULES:
-        imported_module = importlib.import_module("userbot.modules." + module_name)
-        LOGS.info("Your Bot is alive! Test it by typing .alive on any chat." "Should you need assistance, head to https://t.me/TheBotSupport")
-        LOGS.info("Your Bot Version is 2.2-a")
-        if len(sys.argv) not in (1, 3, 4):
-            bot.disconnect()
-    else:
-        await bot.send_message(LOGGER_GROUP, "Connection is Alive!")
-    await bot.run_until_disconnected()
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("userbot.modules." + module_name)
+
+LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
+          "Should you need assistance, head to https://t.me/TheBotSupport")
+LOGS.info("Your Bot Version is 2.2-a")
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
