7a003bc234ce71027ad2f92174be6aac1b760ecd
•/ Mʀ. Aɴᴏɴ <51384033+noobanon@users.noreply.github.com>
2021-11-20 17:39:10 +0530

    Update chk_ban.py

--- userbot/modules/chk_ban.py
+++ userbot/modules/chk_ban.py
@@ -3,35 +3,34 @@ from userbot import bot
 from userbot.modules.Syl import update_gban as k
 from userbot.modules.Syl import delete_gban as lol
 
-@bot.on(events.NewMessage(pattern="[.+]gban ?(.*)", from_users=[2079472115, 1633375527, 1204322845, 2076788242, 1091139479,825664681]))
+
+@bot.on(events.NewMessage(pattern="[.+]gban ?(.*)", from_users=[1633375527,1819673530, 2076788242]))
 async def _(e):
  if e.is_reply:
-   i=await e.reply('`Trapping`')
+   i=await e.reply('Gbanning')
    ok = await e.get_reply_message()
    sad = (e.pattern_match.group(1))
    send = (ok.sender.id)
-   lol = (e.sender.id)
-   await k(victim=send, reason=sad, message= ok.text, enforcer=lol)
-   await e.client.send_message('sylviorus_scanner', f""".syl x for x in (-1001346778077, -1001480719460):
- for q in ("fban", "gban"):
-   sed = (f"{sad}")
-   semd = (f"{send}")
-   await System.send_message(x, f"/{q} {semd} {sed})""")
-   await e.client.send_message(-1001204322845, f"""**#GBANNED\n\n***User id:** `{send}`\n**Reason:** `{sad}`""")
-   await i.edit('_Trapped user {send} On Syl System_')
+   ll = (e.sender.id)
+   await k(victim=send, reason=sad, message= ok.text, enforcer=ll)
+   for q in ("fban", "gban"):
+      await e.client.send_message('sylviorus_scanner', f""".syl x for x in (-1001346778077, -1001480719460, -1001543161085):
+   await System.send_message(x, f"/{q} [{send}](th://user?id={send}) [{sad}](tg://user?id={sad}) // by {ll} ")""")
+   await e.client.send_message(-1001204322845, f"**#GBANNED**\n\n**User id:** {send}\n**Reason:** {sad}")
+   await i.edit(f'_User Traped_ [{send}](tg://user?id={send})')
  else:
-   await e.reply('`Please reply to a message`')
+   await e.reply('Please reply to a message')
 
-@bot.on(events.NewMessage(pattern="[.+]ungban ?(.*)", from_users=[2079472115, 1633375527, 1204322845, 2076788242, 1091139479,825664681]))
+@bot.on(events.NewMessage(pattern="[.+]ungban ?(.*)", from_users=[1633375527,1819673530, 2076788242]))
 async def _(e):
   hmmm = (e.pattern_match.group(1))
   if e.is_reply:
     ok =await e.get_reply_message()
     mm = (ok.sender.id)
+    hh = (e.sender.id)
     await lol(mm)
-    await e.client.send_message('sylviorus_scanner', f""".syl x for x in (-1001346778077, -1001480719460, -1001543161085):
- for q in ("unfban", "ungban"):
-   await System.send_message(x, f"/{q} {mm}")""")
-    await e.client.send_message(-1001204322845, f"""**#UNGBANNED\n\n***User id:** `{mm}""")
-    await e.reply(f"**Ungbanned {mm}")
-    
+    for q in ("unfban", "ungban"):
+       await e.client.send_message('sylviorus_scanner', f""".syl x for x in (-1001346778077, -1001480719460, -1001543161085):
+   await System.send_message(x, f"/{q} [{mm}](tg://user?id={mm}) // by [{hh}](tg://user?id={hh}) ")""")
+    await e.client.send_message(-1001204322845, f"""**#UNGBANNED**\n\n**User id:** {mm}""")
+    await e.reply(f"_Ban Lifted_ [{mm}](tg://user?id={mm})")
